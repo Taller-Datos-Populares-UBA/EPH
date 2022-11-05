6f2d190a3748cb327d049b1ec2aee2f3196a3a90
olaf <arielolafsalgado@gmail.com>
2022-11-05 14:39:16 -0300

    Nueva pestaña incorporada

--- tablero_prototipo/ui.R
+++ tablero_prototipo/ui.R
@@ -2,55 +2,106 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Trabajadorxs de la Economía Popular"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-          
-            checkboxInput("separar_sexos",
-                        "¿Separar por sexo?",
-                        value=FALSE),
-            
-            checkboxInput("separar_zonas",
-                          "¿Separar por zonas?",
-                          value=FALSE),
-            
-            sliderInput("slider_años", 
-                        label = 'Rango de años',
-                        min = 2003, 
-                        max = 2022, 
-                        value = c(2003, 2022),
-                        step = 1,
-                        sep = ''),
-            
-            selectInput(
-              inputId = "variable_zona",
-              label = "Zonas a considerar",
-              choices = c("REGION","AGLOMERADO"),
-              multiple = FALSE,
-              selected = "REGION"),
-            
-            uiOutput("zonas_posibles"),
-
-            selectInput(
-              inputId = "variable_edad",
-              label = "Partición de la edad",
-              choices = c("QUINQUENIO","DECENIO"),
-              multiple = FALSE,
-              selected = "QUINQUENIO"),
-            
-            uiOutput("edades_posibles")
-            
-        ),
-        
-
-        # Gráfico de cantidad de personas participando de la economía popular
-        mainPanel(
-            plotOutput("cantTrabEP_plot")
-        )
-    )
+shinyUI(navbarPage("La Economía Popular",
+  
+  tabPanel("La Economía Popular en el tiempo",
+           
+           
+           # Sidebar with a slider input for number of bins
+           sidebarLayout(
+             sidebarPanel(
+               
+               checkboxInput("separar_sexos",
+                             "¿Separar por sexo?",
+                             value=FALSE),
+               
+               checkboxInput("separar_zonas",
+                             "¿Separar por zonas?",
+                             value=FALSE),
+               
+               sliderInput("slider_años", 
+                           label = 'Rango de años',
+                           min = 2003, 
+                           max = 2022, 
+                           value = c(2003, 2022),
+                           step = 1,
+                           sep = ''),
+               
+               selectInput(
+                 inputId = "variable_zona",
+                 label = "Zonas a considerar",
+                 choices = c("REGION","AGLOMERADO"),
+                 multiple = FALSE,
+                 selected = "REGION"),
+               
+               uiOutput("zonas_posibles"),
+               
+               selectInput(
+                 inputId = "variable_edad",
+                 label = "Partición de la edad",
+                 choices = c("QUINQUENIO","DECENIO"),
+                 multiple = FALSE,
+                 selected = "QUINQUENIO"),
+               
+               uiOutput("edades_posibles")
+               
+             ),
+             
+             
+             # Gráfico de cantidad de personas participando de la economía popular
+             mainPanel(
+               plotOutput("cantTrabEP_plot")
+             )
+           )
+  ),
+  tabPanel("Pobreza e indigencia en la EP",
+           
+           
+           # Sidebar with a slider input for number of bins
+           sidebarLayout(
+             sidebarPanel(
+               
+               checkboxInput("separar_sexos_t2",
+                             "¿Separar por sexo?",
+                             value=FALSE),
+               
+               checkboxInput("separar_zonas_t2",
+                             "¿Separar por zonas?",
+                             value=FALSE),
+               
+               sliderInput("slider_años_t2", 
+                           label = 'Rango de años',
+                           min = 2003, 
+                           max = 2022, 
+                           value = c(2003, 2022),
+                           step = 1,
+                           sep = ''),
+               
+               selectInput(
+                 inputId = "variable_zona_t2",
+                 label = "Zonas a considerar",
+                 choices = c("REGION","AGLOMERADO"),
+                 multiple = FALSE,
+                 selected = "REGION"),
+               
+#               uiOutput("zonas_posibles_t2"),
+               
+               selectInput(
+                 inputId = "variable_edad_t2",
+                 label = "Partición de la edad",
+                 choices = c("QUINQUENIO","DECENIO"),
+                 multiple = FALSE,
+                 selected = "QUINQUENIO"),
+               
+ #              uiOutput("edades_posibles")
+               
+             ),
+             
+             
+             # Gráfico de cantidad de personas participando de la economía popular
+             mainPanel(
+  #             plotOutput("cantTrabEP_plot")
+             )
+           )
+  )
 ))
